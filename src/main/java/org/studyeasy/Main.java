1dd6dd534aedc91048ee1de0c1d7bc4e18bf8bfa
aryansoni25 <ashoksoni9425@gmail.com>
2025-11-08 20:51:47 +0530

    Initial commit

---
+++ src/main/java/org/studyeasy/Main.java
@@ -0,0 +1,17 @@
+package org.studyeasy;
+
+public class Main {
+    public static void main(String[] args) {
+        ThreeInOne TIO=new ThreeInOne(3);
+        TIO.push(0,1);
+        TIO.push(0,2);
+        TIO.push(0,3);
+        TIO.push(1,4);
+        TIO.push(1,5);
+        TIO.push(2,7);
+        TIO.push(2,8);
+        System.out.println(TIO.peek(0));
+        System.out.println(TIO.peek(2));
+        System.out.println(TIO.peek(1));
+    }
+}
---
+++ src/main/java/org/studyeasy/ThreeInOne.java
@@ -0,0 +1,67 @@
+package org.studyeasy;
+
+public class ThreeInOne {
+    private int numberOfStack=3;
+    private int stackCapacity;
+    private int[] values;
+    private int[] sizes;
+
+    public ThreeInOne(int stackSize){
+        stackCapacity=stackSize;
+        values=new int[stackSize*numberOfStack];
+        sizes=new int[numberOfStack];
+    }
+
+    public boolean isFull(int stackNum){
+        if(sizes[stackNum]==stackCapacity){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean isEmpty(int stackNum){
+        if(sizes[stackNum]==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private int indexOfTop(int stackNum){
+        int offset=stackNum*stackCapacity;
+        int size=sizes[stackNum];
+        return offset+size-1;
+    }
+
+    public void push(int stackNum,int value){
+        if(isFull(stackNum)){
+            System.out.println("The Stack id Full!!");
+        }else{
+            sizes[stackNum]++;
+            values[indexOfTop(stackNum)]=value;
+        }
+    }
+
+    public int pop(int stackNum){
+        if(isEmpty(stackNum)){
+            System.out.println("The Stack id Empty!!");
+            return -1;
+        }else{
+            int topOfIndex=indexOfTop(stackNum);
+            int value=values[topOfIndex];
+            values[topOfIndex]=0;
+            sizes[stackNum]--;
+            return value ;
+        }
+    }
+
+    public int peek(int stackNum){
+        if(isEmpty(stackNum)){
+            System.out.println("The Stack id Empty!!");
+            return -1;
+        }else{
+            return values[indexOfTop(stackNum)];
+        }
+    }
+}
